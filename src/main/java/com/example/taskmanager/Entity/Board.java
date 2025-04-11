8d750bea12767280dbdb664369903c1fc838f5a2
Hediye Ersan <hediyesnl9@gmail.com>
2025-04-11 16:26:36 +0300

    Getter ve Setterlar manuel olarak yazılır

--- src/main/java/com/example/taskmanager/Entity/Board.java
+++ src/main/java/com/example/taskmanager/Entity/Board.java
@@ -12,8 +12,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "boards", schema = "taskmanager")
-@Getter
-@Setter
+
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,9 +22,41 @@ public class Board {
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false) // Board her zaman bir User ile ilişkilendirilecekse
     private User user;
 
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<BoardColumn> boardColumns = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<BoardColumn> getBoardColumns() {
+        return boardColumns;
+    }
+
+    public void setBoardColumns(List<BoardColumn> boardColumns) {
+        this.boardColumns = boardColumns;
+    }
 }
