a1839444a6533b6bcecd2c83ec5e913558705b87
Hediye Ersan <hediyesnl9@gmail.com>
2025-04-11 16:29:44 +0300

    TaskMapper entegre edilir

--- src/main/java/com/example/taskmanager/Controller/TaskController.java
+++ src/main/java/com/example/taskmanager/Controller/TaskController.java
@@ -1,6 +1,8 @@
 package com.example.taskmanager.Controller;
 
 import com.example.taskmanager.Dto.TaskDTO;
+import com.example.taskmanager.Dto.TaskMapper;
+import com.example.taskmanager.Entity.Priority;
 import com.example.taskmanager.Entity.Task;
 import com.example.taskmanager.Service.TaskService;
 import com.example.taskmanager.exceptions.ResourceNotFoundException;
@@ -8,7 +10,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -17,20 +21,45 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private TaskMapper taskMapper;
+
     @PostMapping
     public TaskDTO createTask(@RequestBody @Valid TaskDTO taskDTO) {
-        return taskService.createTask(taskDTO); // DTO ile işlem yapıyoruz
+        Task task = taskMapper.toEntity(taskDTO); // DTO'yu Task'e dönüştürüyoruz
+        task = taskService.prepareTask(task); // prepareTask fonksiyonunu çağırıyoruz
+        TaskDTO savedTaskDTO = taskService.createTask(taskMapper.toDTO(task)); // Task'i kaydediyoruz ve DTO'ya dönüştürüyoruz
+        return savedTaskDTO; // Kaydedilen Task DTO olarak döndürülüyor
     }
+
+
 
     @PutMapping("/{id}")
     public TaskDTO updateTask(@PathVariable Long id, @RequestBody @Valid TaskDTO taskDTO) {
         return taskService.updateTask(id, taskDTO); // DTO ile işlem yapıyoruz
     }
 
-    @GetMapping
-    public List<TaskDTO> getAllTasks() {
-        return taskService.getAllTasks(); // DTO'ları döndürüyoruz
+    @PutMapping("/{taskId}/move/{boardColumnId}")
+    public TaskDTO updateTaskColumn(@PathVariable Long taskId, @PathVariable Long boardColumnId) {
+        Task updatedTask = taskService.updateTaskColumn(taskId, boardColumnId);
+        return taskMapper.toDTO(updatedTask); // Güncellenmiş task DTO olarak döndürülür
     }
+
+
+    @GetMapping("/tasks")
+    public List<TaskDTO> getAllTasks() {
+        List<TaskDTO> tasks = taskService.getAllTasks();
+        return tasks; // Burada zaten enum olarak döndürdük
+    }
+
+    @GetMapping("/priorities")
+    public List<String> getAllPriorities() {
+        List<String> priorities = Arrays.stream(Priority.values())
+                .map(Enum::name)  // Enum değerlerini string olarak alıyoruz
+                .collect(Collectors.toList());
+        return priorities;
+    }
+
 
     @GetMapping("/{id}")
     public TaskDTO getTaskById(@PathVariable Long id) {
