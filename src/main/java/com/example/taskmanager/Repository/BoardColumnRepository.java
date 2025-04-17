c5c5efca6e6b46de43de366738d4a58e7a2c3044
Hediye Ersan <hediyesnl9@gmail.com>
2025-04-17 16:19:33 +0300

    Method ismi revize eder

--- src/main/java/com/example/taskmanager/Repository/BoardColumnRepository.java
+++ src/main/java/com/example/taskmanager/Repository/BoardColumnRepository.java
@@ -1,10 +1,11 @@
 package com.example.taskmanager.Repository;
 
 import com.example.taskmanager.Entity.BoardColumn;
+import com.example.taskmanager.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface BoardColumnRepository extends JpaRepository<BoardColumn, Long> {
-    Optional<BoardColumn> findByName(String name);
+    Optional<BoardColumn> findByNameAndUser(String name, User user);
 }
